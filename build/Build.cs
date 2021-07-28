4b8be85fd89dc0c9fa2f05a7bdc4c828fac1bcf2
Ryan Gribble <ryan.gribble@gmail.com>
2021-07-28 16:03:19 +1000

    add a test nuke+octoversion workflow and relevant build project

---
+++ build/Build.cs
@@ -0,0 +1,38 @@
+using Nuke.Common;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using OctoVersion.Core;
+using Nuke.OctoVersion;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+class Build : NukeBuild
+{
+    readonly Configuration Configuration = Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+
+    [NukeOctoVersion] readonly OctoVersionInfo OctoVersionInfo;
+
+    static AbsolutePath SourceDirectory => RootDirectory / "source";
+    static AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    static AbsolutePath PublishDirectory => RootDirectory / "publish";
+    static AbsolutePath LocalPackagesDir => RootDirectory / ".." / "LocalPackages";
+
+    Target CalculateVersion => _ => _
+        .Executes(() =>
+        {
+            //all the magic happens inside `[NukeOctoVersion]` above. we just need a target for TeamCity to call
+        });
+
+    Target Default => _ => _
+        .DependsOn(CalculateVersion);
+
+    /// Support plugins are available for:
+    /// - JetBrains ReSharper        https://nuke.build/resharper
+    /// - JetBrains Rider            https://nuke.build/rider
+    /// - Microsoft VisualStudio     https://nuke.build/visualstudio
+    /// - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main() => Execute<Build>(x => x.Default);
+}
