745daabc88ed52ade952c728708624d159cb3e91
Ryan Gribble <ryangribble@users.noreply.github.com>
2021-08-30 23:08:29 +1000

    Update to new Nuke (#4)
    * update to new nuke version with builtin octoversion tool support
    
    * update to the new versions
    
    * use latest octoversion build
    
    * update output variable names to the new lowercase standard
    
    * more playing with output vars
    
    * rename workflow
    
    * workflow not being detected
    
    * so fun
    
    * so much fun
    
    * round trip
    
    * json
    
    * fix name
    
    * try directly accessing
    
    * add tagging of non pre release builds
    
    * use latest Nuke+OctoVersion
    
    * test run with latest nuke attribute
    
    * change to use latest version with more attributes
    
    * change github actions workflow to remove env vars and pass octoversion parameters instead
    
    * NUKE schema updates
    
    * add line continuation characters to build command

--- build/Build.cs
+++ build/Build.cs
@@ -1,9 +1,9 @@
+using System;
 using Nuke.Common;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using OctoVersion.Core;
-using Nuke.OctoVersion;
+using Nuke.Common.Tools.OctoVersion;
 
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
@@ -11,23 +11,38 @@ class Build : NukeBuild
 {
     readonly Configuration Configuration = Configuration.Release;
 
-    [Solution] readonly Solution Solution;
+    [Parameter] readonly bool? OctoVersionAutoDetectBranch;
+    [Parameter] readonly string OctoVersionBranch;
+    [Parameter] readonly int? OctoVersionMajor;
+    [Parameter] readonly int? OctoVersionMinor;
+    [Parameter] readonly int? OctoVersionPatch;
 
-    [NukeOctoVersion] readonly OctoVersionInfo OctoVersionInfo;
+    [Required]
+    [OctoVersion(
+        AutoDetectBranchParameter = nameof(OctoVersionAutoDetectBranch),
+        BranchParameter = nameof(OctoVersionBranch),
+        MajorParameter = nameof(OctoVersionMajor),
+        MinorParameter = nameof(OctoVersionMinor),
+        PatchParameter = nameof(OctoVersionPatch))]
+    readonly OctoVersionInfo OctoVersionInfo;
 
     static AbsolutePath SourceDirectory => RootDirectory / "source";
     static AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
     static AbsolutePath PublishDirectory => RootDirectory / "publish";
     static AbsolutePath LocalPackagesDir => RootDirectory / ".." / "LocalPackages";
 
-    Target CalculateVersion => _ => _
+    Target OutputVersion => _ => _
         .Executes(() =>
         {
-            //all the magic happens inside `[NukeOctoVersion]` above. we just need a target for TeamCity to call
+            //all the magic happens inside `[OctoVersion]` above.  We can just check if the versions got populated here
+            Console.WriteLine("Outputting OctoVersion calculated values to see if they were calculated correctly within Nuke");
+            Console.WriteLine($"FullSemVer:     {OctoVersionInfo.FullSemVer}");
+            Console.WriteLine($"NuGetVersion:   {OctoVersionInfo.NuGetVersion}");
+            Console.WriteLine($"BuildMetaData:  {OctoVersionInfo.BuildMetaData}");
         });
 
     Target Default => _ => _
-        .DependsOn(CalculateVersion);
+        .DependsOn(OutputVersion);
 
     /// Support plugins are available for:
     /// - JetBrains ReSharper        https://nuke.build/resharper
