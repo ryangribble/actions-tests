02790f076ecc93eba260c6855bf37dc5138b9353
Ryan Gribble <ryan.gribble@gmail.com>
2021-10-20 15:34:08 +1000

    introduce a deliberate exception

--- build/Build.cs
+++ build/Build.cs
@@ -22,6 +22,7 @@ class Build : NukeBuild
     Target OutputVersion => _ => _
         .Executes(() =>
         {
+            throw new Exception("Throwing a test exception to see what happens");
             //all the magic happens inside `[OctoVersion]` above.  We can just check if the versions got populated here
             Console.WriteLine("Outputting OctoVersion calculated values to see if they were calculated correctly within Nuke");
             Console.WriteLine($"FullSemVer:     {OctoVersionInfo?.FullSemVer}");
